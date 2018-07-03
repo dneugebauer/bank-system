1f02f90a74647984d7cfb339269f46859e0654f6
Dean Neugebauer <dneugebauer@live.com>
2018-07-03 15:32:57 -0700

    fixed new_account from duplicate names

--- bank_system.py
+++ bank_system.py
@@ -10,13 +10,16 @@ def random_id(): # Creates "unique" 9-digit account ID
     temp_id = str((unix_temp + random.randint(1,unix_temp))**5)
     return temp_id[-18:-9]
 
-def new_account(name, deposit):
-    unix = time.time()
-    id = random_id()
-    date = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S'))
-    c.execute('INSERT INTO accountbook (accountid, name, balance, opendate) VALUES (?, ?, ?, ?)',
-        (id, name, deposit, date))
-    conn.commit()
+def new_account(client, deposit):
+    if account_lookup(client) != []:
+        print('{} already has an account.'.format(client))
+    else:
+        unix = time.time()
+        id = random_id()
+        date = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S'))
+        c.execute('INSERT INTO accountbook (accountid, name, balance, opendate) VALUES (?, ?, ?, ?) ',
+            (id, client, deposit, date))
+        conn.commit()
 
 def connect_table():
     c.execute('CREATE TABLE IF NOT EXISTS accountbook(accountid TEXT, name TEXT, balance INTEGER , opendate TEXT)')
@@ -24,4 +27,5 @@ def connect_table():
 def account_lookup(client):
     c.execute('SELECT name, balance, accountid, opendate FROM accountbook WHERE name=?', [client])
     data = c.fetchall() # This is the data set for client
-    print("Account '{}' has a balance of {} coins.".format(data[0][0], data[0][1]))
+    #print("Account '{}' has a balance of {}.".format(data[0][0], data[0][1]))
+    return data
